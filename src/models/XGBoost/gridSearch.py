3e695e91cfd9ecc2c5a4a0a30a4fed88e21df896
Zhenglin <zhengli3@ualberta.ca>
2023-04-19 20:02:22 -0600

    multi-thread version of gridSearch

--- src/models/XGBoost/gridSearch.py
+++ src/models/XGBoost/gridSearch.py
@@ -5,6 +5,9 @@ import itertools
 import pandas as pd
 from tqdm import tqdm
 
+import threading
+import queue
+
 from XGBoostClassifier import XGBoostClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
 
@@ -27,6 +30,7 @@ history = {"epoch":[],
             "f1": [],
             }
 
+
 def run(config, data):
     
     x_train, y_train, x_val, y_val, x_test, y_test = data[0], data[1], data[2], data[3], data[4], data[5]
@@ -46,18 +50,17 @@ def run(config, data):
     spe = confusion[0][0] / (confusion[0][1] + confusion[0][0])
     f1 = 2*pre*rec / (pre + rec)
     
-    return {"config": config, "time": time_cost, "confusion": confusion, "acc": acc, "pre": pre, "spe": spe, "f1": f1}
+    return {"time": time_cost, "confusion": confusion, "acc": acc, "pre": pre, "spe": spe, "f1": f1}
     
 
 def load_checkpoint(save_dir, history) -> list:
     if os.path.exists(save_dir):    # load checkpoint if there exists checkpoint
-        checkpoint = pd.read_csv(save_dir, index_col = [0])
+        checkpoint = pd.read_csv(save_dir, index_col = [0])  
         keys = checkpoint.columns
         for key in keys:
             history[key] = list(checkpoint[key])
         
-        return history
-
+    return history
 
 def get_configs(save_dir):
     configs = []
@@ -70,9 +73,32 @@ def get_configs(save_dir):
                 config.append(list(checkpoint[key])[i])
             configs.append((tuple(config)))
     return configs
-         
 
-if __name__ == "__main__":
+
+def convert(h):
+    for key in h.keys():
+        h[key] = [h[key]]
+
+    return h
+
+
+def thread_grid_search(grid, histories, data):
+    while True:
+        config = grid.get()
+        print(threading.current_thread().name, f"config on run: {config}")
+        
+        res = run(config, data)
+        histories.put({**config, **res})
+
+
+def thread_save_history(histories, save_dir):
+    while True:
+        h = histories.get()
+        df = pd.DataFrame.from_dict(convert(h))
+        df.to_csv(save_dir, index=False, header=False, mode='a')
+        
+
+if __name__ == '__main__':
 
     data_dir = './data/train_new.csv'
     yaml_dir = './src/models/XGBoost/config.yaml'
@@ -88,38 +114,35 @@ if __name__ == "__main__":
     history = load_checkpoint(save_dir, history)
     grid_done = get_configs(save_dir)
     
-    grid = []
+    if len(grid_done) == 0 : 
+        df = pd.DataFrame.from_dict(history)
+        df.to_csv(save_dir, index=False)
+    
+    grid = queue.Queue()
+    histories = queue.Queue()
+    
     values = list(configs.values())
     for config in itertools.product(*values):
-        if config in grid_done:
+        if config not in grid_done:
+            grid.put({"epoch": config[0],
+                        "Lambda": config[1],
+                        "Gamma": config[2],
+                        "cover": config[3],
+                        "learning_rate": config[4],
+                        "quantiles": config[5],
+                        "tree_height": config[6],
+                        "feature_ratio": config[7],
+                        "sample_ratio": config[8]
+                        })
+        else:
             continue
-        grid.append({"epoch": config[0],
-                     "Lambda": config[1],
-                     "Gamma": config[2],
-                     "cover": config[3],
-                     "learning_rate": config[4],
-                     "quantiles": config[5],
-                     "tree_height": config[6],
-                     "feature_ratio": config[7],
-                     "sample_ratio": config[8]
-                     })
-    print("total config combinations: %d" % len(grid))
-
-    for i in tqdm(range(len(grid))):
-        print("\nconfig on run: ", grid[i])
-        res = run(grid[i], data)
-        for key in res.keys():
-            if str(key) == "config":
-                for config_key in res[key].keys():
-                    history[config_key].append(res[key][config_key])
-            else:
-                history[key].append(res[key])
-        
-        if i % 10 == 0:     # save history
-            df = pd.DataFrame.from_dict(history)
-            df.to_csv(save_dir)
-
-    df = pd.DataFrame.from_dict(history)
-    df.to_csv(save_dir)
+    print("total config combinations: %d" % grid.qsize())
     
+    for idx in range(2):
+        t = threading.Thread(target=thread_grid_search, args=(grid, histories, data), name=f"--thread_search{idx}--")
+        t.start()
     
+    for idx in range(2):
+        t = threading.Thread(target=thread_save_history, args=(histories, save_dir), name=f"--thread_save: {idx}--")
+        t.start()
+
